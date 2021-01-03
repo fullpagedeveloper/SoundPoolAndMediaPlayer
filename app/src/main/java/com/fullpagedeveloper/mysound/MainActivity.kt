b33ad7a302205ca8f1c7831fe510af6fbdf1116a
MaulanaWest <maulana2ahmad@gmail.com>
2021-01-04 01:50:39 +0700

    media player, service and notification

--- app/src/main/java/com/fullpagedeveloper/mysound/MainActivity.kt
+++ app/src/main/java/com/fullpagedeveloper/mysound/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.fullpagedeveloper.mysound
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.AudioAttributes
 import android.media.AudioManager
 import android.media.MediaPlayer
 import android.media.SoundPool
-import android.os.Build
-import android.os.Bundle
+import android.os.*
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.fullpagedeveloper.mysound.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
     private lateinit var sp: SoundPool
     private var soundId: Int = 0
@@ -20,7 +26,30 @@ class MainActivity : AppCompatActivity() {
     private var mMediaPlayer: MediaPlayer? = null
     private var isReady: Boolean = false
 
-    private lateinit var binding: ActivityMainBinding
+    //media service
+    private val TAG = MainActivity::class.java.simpleName
+    private var mService: Messenger? = null
+    private lateinit var mBoundServiceIntent: Intent
+    private var mServiceBound = false
+
+    /*
+    Service Connection adalah interface yang digunakan untuk menghubungkan antara boundservice dengan activity
+     */
+    private val mServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            mService = Messenger(service)
+            mServiceBound = true
+
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            mService = null
+            mServiceBound = false
+        }
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,27 +81,52 @@ class MainActivity : AppCompatActivity() {
         //media player
         init()
 
+        /*
+       Start service untuk media player
+       */
+        mBoundServiceIntent = Intent(this@MainActivity, MediaService::class.java)
+        mBoundServiceIntent.action = MediaService.ACTION_CREATE
+
+        startService(mBoundServiceIntent)
+        bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
+
         val btnPlay = binding.btnPlay
         val btnStop = binding.btnStop
 
         btnPlay.setOnClickListener {
-            if (!isReady) {
-                mMediaPlayer?.prepareAsync()
-            } else {
-                if (mMediaPlayer?.isPlaying as Boolean) {
-                    mMediaPlayer?.pause()
-                } else {
-                    mMediaPlayer?.start()
+            /*
+            Untuk mengirim perintah play
+            */
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
         }
 
         btnStop.setOnClickListener {
-            if (mMediaPlayer?.isPlaying as Boolean || isReady) {
-                mMediaPlayer?.stop()
-                isReady = false
+            /*
+            Untuk mengirim perintah stop
+            */
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
+                }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(mServiceConnection)
+        mBoundServiceIntent.action = MediaService.ACTION_DESTROY
+
+        startService(mBoundServiceIntent)
     }
 
     private fun init() {
---
+++ app/src/main/java/com/fullpagedeveloper/mysound/MediaPlayerCallback.kt
@@ -0,0 +1,6 @@
+package com.fullpagedeveloper.mysound
+
+interface MediaPlayerCallback {
+    fun onPlay()
+    fun onStop()
+}
